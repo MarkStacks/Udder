77478aa6ecd7806d421205b8f893bb9c8e9abac2
Mark W Stacks <mark@beroldingen.com>
2018-09-28 21:22:15 -0700

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Mark Stacks on 9/28/18.
+//  Copyright © 2018 Mark Stacks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
